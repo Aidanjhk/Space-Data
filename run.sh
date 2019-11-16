1b95ddab1435ef0296ee15c4e931add2fc1889cb
Aidan Hurley-Kalici <57423704+Aidanjhk@users.noreply.github.com>
2019-11-16 07:32:38 -0500

    Updated Coordinates file

---
+++ run.sh
@@ -0,0 +1,8 @@
+start chrome https://docs.google.com/spreadsheets/d/1MKrrC4d5_AtrzAB12u8pg2zIIBK81unkl7jzqkiXW6E/export?format=csv
+sleep 5s
+git init
+git add .
+git commit -m "Updated Coordinates file"
+git remote add Planet_Data - Sheet1.csv
+git push origin master
+
