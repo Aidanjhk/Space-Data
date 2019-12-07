c05212dac0adfd32d4cff91205b044af1976451e
Aidan Hurley-Kalici <57423704+Aidanjhk@users.noreply.github.com>
2019-12-07 11:38:30 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 start chrome https://docs.google.com/spreadsheets/d/1KGNQTs7E8qLLWi2FCfdbDisnIiwao2IKRWMIksAI2p0/export?format=csv
-sleep 5s
+sleep 20s
 git init
 git add .
 git commit -m "Updated Coordinates file"
 git remote add Object_Data - Sheet1.csv
 git push origin master
-
